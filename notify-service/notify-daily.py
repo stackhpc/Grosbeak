f15ef5c71d3ee2ff5f6eae5beb9d0c5ffb536b9c
scrungus <tyler@stackhpc.com>
2023-01-10 12:34:01 +0000

    changed webhook

--- notify-service/notify-daily.py
+++ notify-service/notify-daily.py
@@ -20,7 +20,7 @@ import matplotlib.dates
 # exponential moving average - slowest times of the day 
 
 LOG_FILE = os.environ.get('LOG_FILE', './healthmon.log')
-URL = os.environ.get('WEBHOOK_URL', 'Default')
+URL = os.environ.get('DAILY_WEBHOOK_URL', 'Default')
 BOT_TOKEN = os.environ.get('BOT_TOKEN', 'Default')
 USER_TOKEN = os.environ.get('USER_TOKEN', 'Default')
 SELECTED = os.environ.get('IMAGE_FOR_GRAPHING', 'Ubuntu-22.04')
